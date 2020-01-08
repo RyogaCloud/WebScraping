53c9341a54dcb422803d292b133da77c4c2db534
RyogaTakao <170441089@ccalumni.meijo-u.ac.jp>
2020-01-08 09:55:22 +0900

    完成

--- WebScraping/ WebScraping_requests.py
+++ WebScraping/ WebScraping_requests.py
@@ -1,1 +1,16 @@
-import requsts
+import requests
+import bs4
+
+# response = requests.get("https://review-of-my-life.blogspot.com/")
+# print (response.text)
+
+html_doc = requests.get("https://review-of-my-life.blogspot.com").text
+soup = bs4.BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
+# print(soup.prettify())
+
+# ページのaタグを全て抜き出す
+real_page_tags = soup.find_all("a")
+for tag in real_page_tags:
+    # print(tag)  # タグの取得
+    print(tag.string)  # 取得したタグ内の文字を取得
+    # print(tag.get("href"))  # 取得したタグの属性を取得
